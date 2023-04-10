129a4cb247de77a3ec3b53de0b862676f70cd7ea
rezika <rezikabarech@gmail.com>
2023-04-10 03:16:31 -0400

    UnitTestingSorting

--- algorithm/UnitTestSorting.java
+++ algorithm/UnitTestSorting.java
@@ -1,4 +1,4 @@
-/*package algorithm;
+package algorithm;
 
 import org.junit.Assert;
 
@@ -8,27 +8,65 @@ public class UnitTestSorting {
       This class is about Unit testing for Sorting Algorithm.
     /*
      */
- /*
+
+    //Now implement Unit test for rest of the soring algorithm...................below
+
     public static void main(String[] args) {
-        int [] unSortedArray = {6,9,2,5,1,0,4};
-        int [] sortedArray =   {0,1,2,4,5,6,9};
-        //Create Sort object
-        Sort sort = new Sort();
-        //apply unsorted array to selectionSort.
-        sort.selectionSort(unSortedArray);
-        //verify if the unsorted array is sorted by the selection sort algorithm.
-        try {
-            Assert.assertEquals(sortedArray, unSortedArray);
-        }catch(Exception ex){
-            ex.getMessage();
-        }
+   /* int[] unsortedArray = {6, 9, 2, 5, 1, 0, 4};
+    int[] sortedArray = {0, 1, 2, 4, 5, 6, 9};
 
-        //Now implement Unit test for rest of the soring algorithm...................below
+    Sort sort = new Sort();
+*/
+    /*Selection Sort
+    sort.selectionSort(unsortedArray);
+    try {
+        Assert.assertArrayEquals(sortedArray, unsortedArray);
+    } catch (Exception ex) {
+        ex.getMessage();
+    }
+*/
+    /* Bubble Sort
+    unsortedArray = new int[] {6, 9, 2, 5, 1, 0, 4}; // reset the array
+    sort.bubbleSort(unsortedArray);
+    try {
+        Assert.assertArrayEquals(sortedArray, unsortedArray);
+    } catch (Exception ex) {
+        ex.getMessage();
+    }
+    */
 
+    /*Insertion Sort
+    unsortedArray = new int[] {6, 9, 2, 5, 1, 0, 4}; // reset the array
+    sort.insertionSort(unsortedArray);
+    try {
+        Assert.assertArrayEquals(sortedArray, unsortedArray);
+    } catch (Exception ex) {
+        ex.getMessage();
+    }
+*/
+    /* Merge Sort
+    unsortedArray = new int[] {6, 9, 2, 5, 1, 0, 4}; // reset the array
+    sort.mergeSort(unsortedArray, 0, unsortedArray.length - 1);
+    try {
+        Assert.assertArrayEquals(sortedArray, unsortedArray);
+    } catch (Exception ex) {
+        ex.getMessage();
+    }*/
 
-
-
-
+    /* Quick Sort
+    unsortedArray = new int[] {6, 9, 2, 5, 1, 0, 4}; // reset the array
+    sort.quickSort(unsortedArray, 0, unsortedArray.length - 1);
+    try {
+        Assert.assertArrayEquals(sortedArray, unsortedArray);
+    } catch (Exception ex) {
+        ex.getMessage();
     }
 }
-*/
+}
+}
+}
+}
+}
+ }    */
+    }
+}
